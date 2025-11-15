234de21b8bbdd39d0f44a2c43c55a3c999755f56
Atish Sharma <sharma.atish11@gmail.com>
2025-11-15 15:17:01 +0530

    added new notification-service module

---
+++ notification-service/src/test/java/com/as/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
